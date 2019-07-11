f5b4a7cf0f662d7d2fd471ca18e27e0fd2f6b15f
Carlos Amengual <carlosame644+git@gmail.com>
2019-07-11 19:15:16 +0200

    Initial source import.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# uparser
-A small event parser
+# uParser
+A small event parser.
+
+It is at the core of the css4j NSAC parser, and here is available as a separate
+repository. You do not need this repository to build css4j (it has its own copy
+of it).
+
+To build, just run `mvn`.
---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/*
+ *
+ * Copyright (c) 2017-2019, Carlos Amengual.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Licensed under a BSD-style License. You can find the license here:
+ * https://carte.sourceforge.io/css4j/LICENSE.txt
+ *
+ */
+module io.sf.carte.uparser {
+	exports io.sf.carte.uparser;
+}
